7eff936dfd953a4eee9b108c05b489fb1b0fa80e
Lucas Alves <l.alves001@gmail.com>
2021-02-28 22:10:44 -0300

    🏁 Finshed

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -35,7 +35,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   const seconds = time % 60;
 
   function countdownTime() {
-    return 0.05 * 60;
+    return 25 * 60;
   }
 
   function startCountdown() {
